435080dbf85b826fffc7d28eb5fa431a660555f7
Mark Ponomarenko <mark.ponomarenko@gmail.com>
2021-05-10 23:03:47 +0300

    4th task edited

--- src/test/java/TvNetTest.java
+++ src/test/java/TvNetTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.swing.*;
 import java.util.List;
 
 public class TvNetTest {
@@ -15,7 +16,6 @@ public class TvNetTest {
     private final By HOME_PAGE_COMMENTS = By.xpath(".//span[contains(@class, 'comment')]");
     private final By LOGO = By.xpath(".//a[contains(@class, 'header-logo')]");
     private final By RUS_LANGUAGE_SWITCHER = By.xpath("//div[contains(@class, 'menu-items--top')]//a[contains(.,'RUS')]");
-    //    private final By COMMENTS_COUNT = By.xpath(".//span[contains(@onclick, 'window.l')]");              // for 4** draft
 
     @Test
     public void TaskOneFirstTitleCommentPage() {
@@ -31,15 +31,13 @@ public class TvNetTest {
 //        driver.findElement(RUS_LANGUAGE_SWITCHER).click();
 
         List<WebElement> titles = driver.findElements(HOME_PAGE_TITLE);
-        List<WebElement> comments = driver.findElements(TITLE_COMMENT_BTN);
+//        List<WebElement> comments = driver.findElements(TITLE_COMMENT_BTN);
 
         for (int i = 0; i < 1; i = i + 1) {
             if (!titles.get(i).getText().isEmpty()) {
                 driver.findElement(HOME_PAGE_TITLE).click();
-
-                for (int j = 0; j < 1; j = j + 1) {
-                    driver.findElement(TITLE_COMMENT_BTN).click();
-                }
+                wait.until(ExpectedConditions.elementToBeClickable(TITLE_COMMENT_BTN));
+                driver.findElement(TITLE_COMMENT_BTN).click();
             }
         }
     }
@@ -80,8 +78,8 @@ public class TvNetTest {
 
     }
 
-    @Test
-    public void TaskFourAllTitlesPrint() {
+    @Test                     // not finished - comments appear
+    public void TaskFourTitlesPrint() {
         System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -94,37 +92,49 @@ public class TvNetTest {
 
         List<WebElement> titles = driver.findElements(HOME_PAGE_TITLE);
 
-        System.out.println("-------------------------------------------------");
-        System.out.println("HW task #4* - Tvnet list of all titles: ");
+        System.out.println("---------------------------------------");
+        System.out.println("HW task #4 - Tvnet list of all titles: ");
 
         for (int i = 0; i < titles.size(); i = i + 1) {
             if (!titles.get(i).getText().isEmpty()) {
-                System.out.println(i + ". " + titles.get(i).getText());
+                System.out.println(i + 1 + ". " + titles.get(i).getText());
             }
         }
     }
-//
-//    @Test
-//    public void TaskFourAllTitlesPrintWithComments() {
-//        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
-//        WebDriver driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.get("http://tvnet.lv");
-//
-//        WebDriverWait wait = new WebDriverWait(driver, 5);
-//        wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
-//
-//        driver.findElement(ACCEPT_COOKIES_BTN).click();
-//
-//        List<WebElement> titles = driver.findElements(HOME_PAGE_TITLE);
-//
-//        System.out.println("-------------------------------------------------");
-//        System.out.println("HW task #4* - Tvnet list of all titles: ");
-//
-//        for (int i = 0; i < titles.size(); i = i + 1) {
-//            if (!titles.get(i).getText().isEmpty()) {
-//                System.out.println(i + ". " + titles.get(i).getText());
-//            }
+
+    @Test                // not finished
+    public void TaskFiveTitlesPrintWithCommentsCount() {
+        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://tvnet.lv");
+
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
+
+        driver.findElement(ACCEPT_COOKIES_BTN).click();
+
+        List<WebElement> titles = driver.findElements(HOME_PAGE_TITLE);
+        List<WebElement> comments = driver.findElements(HOME_PAGE_COMMENTS);
+
+        System.out.println("------------------------------------------------------------");
+        System.out.println("HW task #5 - Tvnet list of all titles with comments count: ");
+
+//        for (WebElement we : comments) {
+//            System.out.println(we.getText().isEmpty() ? "0" : we.getText());
 //        }
-//    }
+
+        for (int i = 0; i < comments.size(); i = i + 1) {
+            if (!comments.get(i).getText().isEmpty()) {
+                System.out.println(i + 1 + ". " + comments.get(i).getText());
+
+//                for (int j = 0; j < comments.size(); j = j + 1) {
+//                    if (!comments.get(j).getText().isEmpty()) {
+//
+//                        System.out.println(i + 1 + ". " + titles.get(i).getText() + " | comments " + comments.get(i).getText());
+//                    }
+//                }
+            }
+        }
+    }
 }
